feb3c60d3a852aad45a11429a80120f445221eb3
liangbe <9169lucifer@163.com>
2017-12-15 18:03:01 +0800

    shiro setting

--- src/main/java/com/bee/fetch/controller/testController.java
+++ src/main/java/com/bee/fetch/controller/testController.java
@@ -3,16 +3,24 @@ package com.bee.fetch.controller;
 import com.bee.fetch.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by liangbe on 2017/12/13.
  */
 @Controller
+@RequestMapping("/")
 public class testController {
 
     @Autowired
     UserService  userService;
 
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String Login() {
+        return "login";
+    }
 
 
 }
